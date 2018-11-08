918df4a160024b627ffb482c2cb1479614b74468
kim <myung@naver.com>
2018-11-08 12:15:29 +0900

    181108 제발

---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -0,0 +1,143 @@
+package com.model2.mvc.web.product;
+
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductService;
+
+
+//==> 회원관리 Controller
+@Controller
+public class ProductController {
+	
+	///Field
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	//setter Method 구현 않음
+		
+	public ProductController(){
+		System.out.println(this.getClass());
+	}
+	
+	//==> classpath:config/common.properties  ,  classpath:config/commonservice.xml 참조 할것
+	//==> 아래의 두개를 주석을 풀어 의미를 확인 할것
+	@Value("#{commonProperties['pageUnit']}")
+	//@Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+	
+	@Value("#{commonProperties['pageSize']}")
+	//@Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
+	
+	
+	@RequestMapping("/addProductView.do")
+	public String addProductView() throws Exception {
+
+		System.out.println("/addProductView.do");
+		
+		return "redirect:/product/addProductView.jsp";
+	}
+	
+	@RequestMapping("/addProduct.do")
+	public String addProduct( @ModelAttribute("product") Product product ) throws Exception {
+
+		System.out.println("/addProduct.do");
+		//Business Logic
+		productService.insertProduct(product);
+		
+		return "forward:/product/addProduct.jsp";
+	}
+	
+	@RequestMapping("/getProduct.do")
+	public String getProduct( @RequestParam("prodNo") int prodNo , Model model, HttpServletRequest request, HttpServletResponse response ) throws Exception {
+		
+		System.out.println("/getProduct.do");
+		//Business Logic
+		Product product = productService.findProduct(prodNo);
+		// Model 과 View 연결
+		model.addAttribute("product", product);
+		
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null && cookies.length > 0) {
+			System.out.println("기존 Cookie 이용");
+			for (int i = 0; i < cookies.length; i++) {
+				Cookie cookie = cookies[i];
+				if (cookie.getName().equals("history")) {
+					cookie.setValue(cookie.getValue() + "," + prodNo);
+					cookie.setMaxAge(60 * 60);
+					response.addCookie(cookie);
+				} else {
+					System.out.println("Cookie 첫 생성");
+					cookie = new Cookie("history", String.valueOf(prodNo));
+					cookie.setMaxAge(60 * 60);
+					response.addCookie(cookie);
+				}
+			}
+		}
+		
+		return "forward:/product/getProduct.jsp";
+	}
+	
+	@RequestMapping("/updateProductView.do")
+	public String updateProductView( @RequestParam("prodNo") int prodNo , Model model ) throws Exception{
+
+		System.out.println("/updateProductView.do");
+		//Business Logic
+		Product product = productService.findProduct(prodNo);
+		// Model 과 View 연결
+		model.addAttribute("product", product);
+		
+		return "forward:/product/updateProductView.jsp";
+	}
+	
+	@RequestMapping("/updateProduct.do")
+	public String updateProduct( @ModelAttribute("product") Product product , Model model) throws Exception{
+
+		System.out.println("/updateProduct.do");
+		//Business Logic
+		productService.updateProduct(product);
+		
+		return "redirect:/getProduct.do?prodNo="+product.getProdNo();
+	}
+	
+	@RequestMapping("/listProduct.do")
+	public String listProduct( @ModelAttribute("search") Search search , Model model , HttpServletRequest request) throws Exception{
+		
+		System.out.println("/listProduct.do");
+		
+		if(search.getCurrentPage() ==0 ){
+			search.setCurrentPage(1);
+		}
+		search.setPageSize(pageSize);
+		
+		// Business logic 수행
+		Map<String , Object> map=productService.getProductList(search);
+		
+		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+		System.out.println(resultPage);
+		
+		// Model 과 View 연결
+		model.addAttribute("list", map.get("list"));
+		model.addAttribute("resultPage", resultPage);
+		model.addAttribute("search", search);
+		
+		return "forward:/product/listProduct.jsp";
+	}
+}
---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/test/java/com/model2/mvc/service/product/test/ProductServiceTest.java
@@ -0,0 +1,218 @@
+package com.model2.mvc.service.product.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductDao;
+import com.model2.mvc.service.product.ProductService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration	(locations = {	"classpath:config/context-common.xml",
+		"classpath:config/context-aspect.xml",
+		"classpath:config/context-mybatis.xml",
+		"classpath:config/context-transaction.xml" })
+public class ProductServiceTest {
+
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+
+	@Autowired
+	@Qualifier("productDaoImpl")
+	private ProductDao productDao;
+
+	// @Test
+	public void testAddProduct() throws Exception {
+		Product product = new Product();
+		// product.setProdNo(1);
+		product.setProdName("물건이름임");
+		product.setProdDetail("이거슨 Add 테스트임");
+		product.setManuDate("20170101");
+		product.setPrice(10000);
+		product.setFileName("이미지 없어");
+
+		productService.insertProduct(product);
+
+		product = productService.findProduct(1);
+
+		System.out.println(product.toString());
+
+		// Assert.assertEquals(1, product.getProdNo());
+		Assert.assertEquals("물건이름임", product.getProdName());
+		Assert.assertEquals("이거슨 Add 테스트임", product.getProdDetail());
+		Assert.assertEquals("20170101", product.getManuDate());
+		Assert.assertEquals(10000, product.getPrice());
+		Assert.assertEquals("이미지 없어", product.getFileName());
+	}
+
+	//@Test
+	public void testUpdateProduct() throws Exception {
+
+		Product product = productService.findProduct(1);
+		Assert.assertNotNull(product);
+
+		Assert.assertEquals("물건이름임", product.getProdName());
+		Assert.assertEquals("이거슨 Add 테스트임", product.getProdDetail());
+		
+		product.setProdName("업데이트물건이름이지");
+		product.setProdDetail("이거슨 Update 테스트임");
+		
+		productService.updateProduct(product);
+
+		product = productService.findProduct(1);
+		Assert.assertNotNull(product);
+
+		Assert.assertEquals("업데이트물건이름이지", product.getProdName());
+		Assert.assertEquals("이거슨 Update 테스트임", product.getProdDetail());
+	}
+	
+	//@Test
+	public void testGetProductListAll() throws Exception{
+		 
+	 	Search search = new Search();
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	Map<String,Object> map = productService.getProductList(search);
+	 	
+	 	List<Object> list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(3, list.size());
+	 	
+		//==> console 확인
+	 	//System.out.println(list);
+	 	
+	 	Integer totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 	
+	 	System.out.println("=======================================");
+	 	
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	search.setSearchCondition("0");
+	 	search.setSearchKeyword("");
+	 	map = productService.getProductList(search);
+	 	
+	 	list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(3, list.size());
+	 	
+	 	//==> console 확인
+	 	//System.out.println(list);
+	 	
+	 	totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 }
+	
+		//@Test
+		 public void testGetProductListByProductNo() throws Exception{
+			 
+		 	Search search = new Search();
+		 	search.setCurrentPage(1);
+		 	search.setPageSize(3);
+		 	search.setSearchCondition("0");
+		 	search.setSearchKeyword("1");
+		 	Map<String,Object> map = productService.getProductList(search);
+		 	
+		 	List<Object> list = (List<Object>)map.get("list");
+		 	Assert.assertEquals(1, list.size());
+		 	
+			//==> console 확인
+		 	//System.out.println(list);
+		 	
+		 	Integer totalCount = (Integer)map.get("totalCount");
+		 	System.out.println(totalCount);
+		 	
+		 	System.out.println("=======================================");
+		 	
+		 	search.setSearchCondition("0");
+		 	search.setSearchKeyword(""+System.currentTimeMillis());
+		 	map = productService.getProductList(search);
+		 	
+		 	list = (List<Object>)map.get("list");
+		 	Assert.assertEquals(0, list.size());
+		 	
+			//==> console 확인
+		 	//System.out.println(list);
+		 	
+		 	totalCount = (Integer)map.get("totalCount");
+		 	System.out.println(totalCount);
+		 }
+		 
+		//@Test
+		 public void testGetProductListByProductName() throws Exception{
+			 
+		 	Search search = new Search();
+		 	search.setCurrentPage(1);
+		 	search.setPageSize(3);
+		 	search.setSearchCondition("1");
+		 	search.setSearchKeyword("연꽃");
+		 	Map<String,Object> map = productService.getProductList(search);
+		 	
+		 	List<Object> list = (List<Object>)map.get("list");
+		 	Assert.assertEquals(1, list.size());
+		 	
+			//==> console 확인
+		 	//System.out.println(list);
+		 	
+		 	Integer totalCount = (Integer)map.get("totalCount");
+		 	System.out.println(totalCount);
+		 	
+		 	System.out.println("=======================================");
+		 	
+		 	search.setSearchCondition("0");
+		 	search.setSearchKeyword(""+System.currentTimeMillis());
+		 	map = productService.getProductList(search);
+		 	
+		 	list = (List<Object>)map.get("list");
+		 	Assert.assertEquals(0, list.size());
+		 	
+			//==> console 확인
+		 	//System.out.println(list);
+		 	
+		 	totalCount = (Integer)map.get("totalCount");
+		 	System.out.println(totalCount);
+		 }
+		 
+		//@Test
+		 public void testGetProductListByProductPrice() throws Exception{
+			 
+		 	Search search = new Search();
+		 	search.setCurrentPage(1);
+		 	search.setPageSize(3);
+		 	search.setSearchCondition("2");
+		 	search.setSearchKeyword("10000");
+		 	Map<String,Object> map = productService.getProductList(search);
+		 	
+		 	List<Object> list = (List<Object>)map.get("list");
+		 	Assert.assertEquals(1, list.size());
+		 	
+			//==> console 확인
+		 	//System.out.println(list);
+		 	
+		 	Integer totalCount = (Integer)map.get("totalCount");
+		 	System.out.println(totalCount);
+		 	
+		 	System.out.println("=======================================");
+		 	
+		 	search.setSearchCondition("0");
+		 	search.setSearchKeyword(""+System.currentTimeMillis());
+		 	map = productService.getProductList(search);
+		 	
+		 	list = (List<Object>)map.get("list");
+		 	Assert.assertEquals(0, list.size());
+		 	
+			//==> console 확인
+		 	//System.out.println(list);
+		 	
+		 	totalCount = (Integer)map.get("totalCount");
+		 	System.out.println(totalCount);
+		 }
+}
